d5e89f0a1e34b943bdf931d1edb53ec045e4d3f6
jjsuperspy <jj.superspy@hotmail.co.uk>
2020-02-09 00:08:10 +0000

    Added custom renderer
    Add a low resolution renderer.

---
+++ Stealth_Game/Assets/Code/Rendering/LowResRenderer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowResRenderer : MonoBehaviour
+{
+
+    public Camera[] allCameras;
+    private RenderTexture[] allRendTexs;
+    private int yRes = 200;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupRenderTextures();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), allRendTexs[0], ScaleMode.StretchToFill);
+    }
+
+    void SetupRenderTextures()
+    {
+        allRendTexs = new RenderTexture[allCameras.Length];
+        //
+        int ratio = Screen.height / yRes;
+        int newScreenWidth = Screen.width / ratio;
+        int newScreenHeight = Screen.height / ratio;
+        //
+        for (int i = 0; i < allRendTexs.Length; i++)
+        {
+            allRendTexs[i] = new RenderTexture(newScreenWidth, newScreenHeight, 0, RenderTextureFormat.ARGB32);
+
+            allRendTexs[i].antiAliasing = 1;
+            allRendTexs[i].filterMode = FilterMode.Point;
+            allRendTexs[i].isPowerOfTwo = false;
+            //
+            allCameras[i].targetTexture = allRendTexs[i];
+
+        }
+    }
+
+
+}
